96b80367fbbbb144f978e490a776ff7fd6802539
David C Wang <davwang4@cisco.com>
2016-03-05 06:58:53 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# install via:
+#   curl https://raw.githubusercontent.com/dcwangmit01/yq/master/install.sh | sudo bash
+
 sudo apt-get -y install jq
+curl https://raw.githubusercontent.com/dcwangmit01/yq/master/yq > /usr/local/bin/yq
+sudo chmod 755 /usr/local/bin/yq
+
+
 
